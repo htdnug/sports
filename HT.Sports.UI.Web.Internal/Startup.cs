b84f16b40d52524bf0c4e132d7e63baac6054429
Jeremy <jeremy@theknights.me>
2019-01-28 18:07:27 -0600

    added additional comments to Startup.cs

--- HT.Sports.UI.Web.Internal/Startup.cs
+++ HT.Sports.UI.Web.Internal/Startup.cs
@@ -23,11 +23,39 @@ namespace HT.Sports.UI.Web.Internal
         {
             if (env.IsDevelopment())
             {
+                // When the app runs in the Development environment:
+                //   Use the Developer Exception Page to report app runtime errors.
+                //   Use the Database Error Page to report database runtime errors.
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                // When the app doesn't run in the Development environment:
+                //   Enable the Exception Handler Middleware to catch exceptions
+                //app.UseExceptionHandler("/Error");
             }
 
             app.UseStatusCodePages();
+
+            // Use HTTPS Redirection Middleware to redirect HTTP requests to HTTPS.
+            //app.UseHttpsRedirection();
+
+            // Return static files and end the pipeline.
             app.UseStaticFiles();
+
+            // Use Cookie Policy Middleware to conform to EU General Data 
+            // Protection Regulation (GDPR) regulations.
+            //app.UseCookiePolicy();
+
+            // Authenticate before the user accesses secure resources.
+            //app.UseAuthentication();
+
+            // If the app uses session state, call Session Middleware after Cookie 
+            // Policy Middleware and before MVC Middleware.
+            //app.UseSession();
+
+            // Add MVC to the request pipeline.
             //app.UseMvcWithDefaultRoute();
             app.UseMvc(routes =>
             {
