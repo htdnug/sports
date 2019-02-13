bebf354534f790e71d5b606cdf3940584b5c3938
Jeremy <jeremy@theknights.me>
2019-02-12 22:02:12 -0600

    added index pages; updated layout pages; added css file

---
+++ HT.Sports.UI.Web.Internal/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HT.Sports.UI.Web.Internal.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
