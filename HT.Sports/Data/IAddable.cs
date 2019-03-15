61b23368af232a9ed45033ca97f766d6cd708ee0
Devin Duplantis <37842419+dduplantis@users.noreply.github.com>
2019-03-15 12:59:23 -0500

    Implement IAddable Interface
    For universal "can add" contract.

---
+++ HT.Sports/Data/IAddable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT.Sports.Data.EF
+{
+    public interface IAddable<TEntity>
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+    }
+}
