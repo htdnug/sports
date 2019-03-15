800a905d77cb20c2c8d6959cf4ad5253aff995da
Devin Duplantis <37842419+dduplantis@users.noreply.github.com>
2019-03-15 13:01:52 -0500

    Add CreateAsync method to TripService
    Encapsulate repository create method.

--- HT.Sports/Services/TripService.cs
+++ HT.Sports/Services/TripService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using HT.Sports.Data;
 using HT.Sports.Data.EF;
+using HT.Sports.Entities;
 using HT.Sports.Services.Contracts;
 
 namespace HT.Sports.Services
@@ -19,6 +20,12 @@ namespace HT.Sports.Services
         public async Task<int> DeleteAsync(int id)
         {
             await this._tripRepo.DeleteAsync(id);
+            return await this._context.SaveChangesAsync();
+        }
+
+        public async Task<int> CreateAsync(Trip trip)
+        {
+            await this._tripRepo.AddAsync(trip);
             return await this._context.SaveChangesAsync();
         }
     }
