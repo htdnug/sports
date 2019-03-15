66436464c958f8b4d9cd2a20456bc7e0f3924a3c
Devin Duplantis <37842419+dduplantis@users.noreply.github.com>
2019-03-15 12:50:23 -0500

    Create "CreateAsyncOperation"
    For universal async method usage.

---
+++ HT.Sports/Data/EF/Operations/CreateAsyncOperation.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using HT.Sports.Entities;
+
+namespace HT.Sports.Data.EF.Operations
+{
+    internal class AddAsyncOperation<TRepo, TEntity>
+       where TEntity : EntityBase
+       where TRepo : RepoBase<TEntity>, IReadableById<int, TEntity>
+    {
+        private readonly TRepo _repo;
+
+        public AddAsyncOperation(TRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            var result = await _repo.Context.Set<TEntity>().AddAsync(entity);
+            return result.Entity;
+        }
+    }
+}
