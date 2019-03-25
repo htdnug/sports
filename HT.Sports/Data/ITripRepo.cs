a99a130f52b5598e07d2252187fc2a0f845fb110
Devin Duplantis <37842419+dduplantis@users.noreply.github.com>
2019-03-25 10:37:15 -0500

    Add contracts to ITripRepo
    This defines what functions the trip repository can perform.

--- HT.Sports/Data/ITripRepo.cs
+++ HT.Sports/Data/ITripRepo.cs
@@ -4,15 +4,16 @@ using System.Threading.Tasks;
 using HT.Sports.Data.EF;
 using HT.Sports.Entities;
 
+
 namespace HT.Sports.Data
 {
     public interface ITripRepo : 
-        IDeleteable<int>, 
+        IDeletable<int>, 
+        IUpdateable<Trip>,
         IAddable<Trip>,
         IReadableById<int, Trip>
     {
         Task<bool> DuplicateExistsAsync(Trip trip);
         Task<List<Trip>> GetAllAsync();
-        Task<int> UpdateAsync(Trip trip, Action<Trip, Trip> propertyCopyAction);
     }
 }
