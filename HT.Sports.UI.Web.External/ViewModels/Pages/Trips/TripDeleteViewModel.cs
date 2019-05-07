7f80e4823e41382af28c28b4dbeb71a8efb12fdf
Devin Duplantis <dduplantis17@its.nicholls.edu>
2019-05-07 17:20:00 -0500

    Create TripDeleteViewModel, Update Trip Delete Page
    In Delete.cshtml:
    Use TripDeleteViewModel instead of PageModel, accomodate extra ViewModel attributes.
    
    In Delete.cshtml.cs:
    Remove bindable attributes and bind to TripDeleteViewModel.
    Remove trip repo in favor of trip service, replace repo calls with service calls.

---
+++ HT.Sports.UI.Web.External/ViewModels/Pages/Trips/TripDeleteViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using HT.Sports.Entities;
+
+namespace HT.Sports.UI.Web.External.ViewModels.Pages.Trips
+{
+    public class TripDeleteViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Trip Type")]
+        public TripTypes TripType { get; set; }
+
+        [Display(Name = "Trip Start Longitude (X)")]
+        [Range(-180, 180)]
+        public double TripStartLongitude { get; set; }
+
+        [Range(-90, 90)]
+        [Display(Name = "Trip Start Latitude (Y)")]
+        public double TripStartLatitude { get; set; }
+
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        [DataType(DataType.Date)]
+        public DateTime DateOccurred { get; set; }
+
+        [Display(Name = "Created by user")]
+        public string UserName { get; set; }
+    }
+}
