575253efe8c4365fdec1498c8e4a2ba1ec569292
Devin Duplantis <dduplantis17@its.nicholls.edu>
2019-05-07 16:58:52 -0500

    Update TripIndexViewModel, Trip Index Page
    In TripIndexViewModel:
    Add new Trip attributes
    
    In Index.cshtml:
    Add new ViewModel attributes
    
    In Index.cshtml.cs:
    Remove trip repo in favor of trip service.
    Add new attributes to ViewModel on get method.
    Use new service methods for getting trips and related data, and a single entity for reading data attributes such as display names and ranges (might not be best practice).

--- HT.Sports.UI.Web.External/ViewModels/Pages/Trips/TripIndexViewModel.cs
+++ HT.Sports.UI.Web.External/ViewModels/Pages/Trips/TripIndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using HT.Sports.Entities;
 
 namespace HT.Sports.UI.Web.External.ViewModels.Pages.Trips
 {
@@ -7,8 +8,20 @@ namespace HT.Sports.UI.Web.External.ViewModels.Pages.Trips
     {
         public int Id { get; set; }
 
+        [Display(Name = "Trip Type")]
+        public TripTypes TripType { get; set; }
+
+        [Display(Name = "Trip Start Longitude")]
+        public double TripStartLongitude { get; set; }
+
+        [Display(Name = "Trip Start Latitude")]
+        public double TripStartLatitude { get; set; }
+
         [Display(Name = "Date")]
         [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime DateOccurred { get; set; }
+
+        [Display(Name = "Created by user")]
+        public string UserName { get; set; }
     }
 }
