a20b34e99b955bac5a123e3ad3306b314e48ae89
Devin Duplantis <dduplantis17@its.nicholls.edu>
2019-05-07 16:27:07 -0500

    Update Trip Entity
    Add TripType enum.
    Add Trip type and Trip coordinate attributes.

--- HT.Sports/Entities/Trip.cs
+++ HT.Sports/Entities/Trip.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HT.Sports.Entities
 {
+
+    public enum TripTypes {None = 0, Fishing = 1, Hunting = 2}
+
     public class Trip : EntityBase<int>
 
     {
-        public DateTime DateOccurred { get; set; }
+        public TripTypes TripType { get; set; }
+        public double TripStartLongitude { get; set; }
+        public double TripStartLatitude { get; set; }
         public int UserProfileId { get; set; }
+        public DateTime DateOccurred { get; set; }       
         public UserProfile UserProfile { get; set; }
     }
 }
